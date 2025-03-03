01d966054dffcf308108444e214316e8a0d0b1be
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-03-03 22:57:34 +0100

    board

---
+++ src/components/Board/WhiteBoard.tsx
@@ -0,0 +1,10 @@
+const Whiteboard = () => {
+    return (
+      <div className="flex-grow bg-gray-100 min-h-screen relative p-4">
+        <div className="w-full h-full  board"></div>
+      </div>
+    );
+  };
+  
+  export default Whiteboard;
+  
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,10 +1,14 @@
 import TopBar from "../components/Board/Top"
+import Whiteboard from "../components/Board/WhiteBoard"
 
 
 const Board = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <TopBar />
+      <div className="relative flex flex-grow">
+        <Whiteboard />
+      </div>
     </div>
   )
 }
