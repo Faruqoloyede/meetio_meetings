526ec5c48de814e50197214a4f94a24c47f1b791
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-03-07 17:23:10 +0100

    added tools

--- src/components/whiteBoard/ToolBar.tsx
+++ src/components/whiteBoard/ToolBar.tsx
@@ -1,12 +1,19 @@
+import { MdOutlineRectangle } from "react-icons/md";
+import { GiArrowCursor } from "react-icons/gi";
+import { RxText } from "react-icons/rx";
+import { BiImage } from "react-icons/bi";
+import { HiOutlineLink } from "react-icons/hi2";
+import { BiSolidMessageRounded } from "react-icons/bi";
+
 const ToolBar = () => {
     return (
-      <div className="bg-teal-600 p-3 flex flex-col rounded-lg space-y-4 absolute top-32 z-40 left-10">
-        <button className="text-black p-2">✏️</button>
-        <button className="text-black p-2">🔤</button>
-        <button className="text-black p-2">🖼️</button>
-        <button className="text-black p-2">🔗</button>
-        <button className="text-black p-2">💬</button>
-        <button className="text-black p-2">hello</button>
+      <div className="bg-teal-600 p-3 flex flex-col items-center rounded-lg space-y-4 absolute top-32 z-40 left-10">
+        <button className="text-white text-2xl p-2"><GiArrowCursor /></button>
+        <button className="text-white text-2xl p-2"><MdOutlineRectangle /></button>
+        <button className="text-white text-2xl p-2"><RxText /></button>
+        <button className="text-white text-2xl p-2"><BiImage /></button>
+        <button className="text-white text-2xl p-2"><HiOutlineLink /></button>
+        <button className="text-white text-2xl p-2"><BiSolidMessageRounded /></button>
       </div>
     );
   };
